8d87cfb56dcf2fc7d42be82141c59e7eff2e08bc
alnesterenko <alzpupkin@gmail.com>
2023-11-09 18:45:15 +0200

    4. Утверждения с исключениями [#504886]

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void dontHaveСorrectSimbol() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse(new String[]{"key:value"}))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("key:value");
+    }
+
+    @Test
+    void simbolAtTheBeginning() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse(new String[]{"=value"}))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("=value");
+    }
+
+    @Test
+    void simbolAtTheEnding() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse(new String[]{"key="}))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("key=");
+    }
+
+    @Test
+    void whenArrayIsEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse(new String[]{}))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("is empty");
+    }
+}
