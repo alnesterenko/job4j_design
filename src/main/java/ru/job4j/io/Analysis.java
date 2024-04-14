5bb22dfa8b50391b4a072377d2cc2eb07dd97bfb
alnesterenko <alzpupkin@gmail.com>
2024-04-14 21:24:40 +0300

    7. Анализ доступности сервера [#859 #491288]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -6,34 +6,30 @@ import java.util.List;
 
 public class Analysis {
     public void unavailable(String source, String target) {
-        StringBuilder stringBuilder = new StringBuilder();
-        List<String> linesBuffer = new ArrayList<>();
-        String start = "", finish = "";
-        boolean serverOnline = true;
-        try (BufferedReader input = new BufferedReader(new FileReader(source))) {
-            linesBuffer = input.lines().filter(line -> line.matches("\\d{3}\s\\d{2}:\\d{2}:\\d{2}")).toList();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        for (String oneLine : linesBuffer) {
-            String[] stringsTempArr = oneLine.split(" ", 2);
-            if (serverOnline) {
-                if (Integer.parseInt(stringsTempArr[0]) > 399) {
-                    start = stringsTempArr[1];
-                    serverOnline = false;
+        String[] startFinish = {"", ""};
+        boolean[] serverOnline = {true};
+        try (BufferedReader input = new BufferedReader(new FileReader(source));
+             BufferedWriter output = new BufferedWriter(new FileWriter(target))) {
+            input.lines().filter(line -> line.matches("\\d{3}\s\\d{2}:\\d{2}:\\d{2}")).forEach((line) -> {
+                String[] stringsTempArr = line.split(" ", 2);
+                if (serverOnline[0]) {
+                    if (Integer.parseInt(stringsTempArr[0]) > 399) {
+                        startFinish[0] = stringsTempArr[1];
+                        serverOnline[0] = false;
+                    }
+                } else if (Integer.parseInt(stringsTempArr[0]) < 400) {
+                    startFinish[1] = stringsTempArr[1];
+                    serverOnline[0] = true;
+                    try {
+                        output.write(String.format("%s;%s;%n", startFinish[0], startFinish[1]));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
-            } else if (Integer.parseInt(stringsTempArr[0]) < 400) {
-                finish = stringsTempArr[1];
-                serverOnline = true;
-                stringBuilder.append(String.format("%s;%s;%n", start, finish));
-            }
-        }
-        try (BufferedWriter output = new BufferedWriter(new FileWriter(target))) {
-            output.write(stringBuilder.toString());
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
     public static void main(String[] args) {
         Analysis analysis = new Analysis();
