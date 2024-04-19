9900663189ca939b70fdd330a66cb38596b0ff67
alnesterenko <alzpupkin@gmail.com>
2024-04-19 14:50:17 +0300

    11. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -0,0 +1,44 @@
+package ru.job4j.io.duplicates;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    private HashMap<FileProperty, HashSet<Path>> resultMap = new HashMap<>();
+    @Override
+    public FileVisitResult visitFile(Path file,
+                                     BasicFileAttributes attributes) throws IOException {
+        File tempFile = file.toFile();
+        if (tempFile.isFile()) {
+            FileProperty tempFileProperty = new FileProperty(tempFile.length(), tempFile.getName());
+            if (resultMap.containsKey(tempFileProperty)) {
+                HashSet tempSet = resultMap.get(tempFileProperty);
+                tempSet.add(file.toAbsolutePath());
+                resultMap.put(tempFileProperty, tempSet);
+            } else {
+                resultMap.put(tempFileProperty, new HashSet<Path>(Arrays.asList(file.toAbsolutePath())));
+            }
+        }
+        return super.visitFile(file, attributes);
+    }
+    
+    public void printDuplicates() {
+        for (Map.Entry<FileProperty, HashSet<Path>> entry : resultMap.entrySet()) {
+            if (entry.getValue().size() > 1) {
+                FileProperty tempFileProperty = entry.getKey();
+                System.out.printf("%s - %.2f Кб%n", tempFileProperty.getName(), (double) tempFileProperty.getSize() / 1024);
+                for (Path path : entry.getValue()) {
+                    System.out.println("\t" + path.toAbsolutePath());
+                }
+            }
+        }
+    }
+}
