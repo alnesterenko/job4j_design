771eb646455ad36de7249fbb0fa12c27fe0f4433
alnesterenko <alzpupkin@gmail.com>
2024-04-27 13:57:50 +0300

    Потоки объектов. (Object streams)(Сериализация) [#504992]

---
+++ src/main/java/ru/job4j/io/objectstream/ObjectStream.java
@@ -0,0 +1,28 @@
+package ru.job4j.io.objectstream;
+
+import java.io.*;
+
+public class ObjectStream {
+
+    public static void main(String[] args) { // здесь сериализация и сразу же десериализация
+        Car car = new Car("Фирма", "Модель", 2000);
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("data/serialized.dat"));
+             ObjectInputStream in = new ObjectInputStream(new FileInputStream("data/serialized.dat"))) {
+            out.writeObject(car);
+            Car deserialized = (Car) in.readObject();
+            System.out.println(deserialized.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*public static void main(String[] args) { // здесь только сериализация
+        Car car = new Car("Фирма", "Модель", 2000);
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("data/serialized.dat"))) {
+            out.writeObject(car);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }*/
+
+}
