f56440fc726e22c624790c9c7827c85f69749f10
alnesterenko <alzpupkin@gmail.com>
2024-07-09 20:57:35 +0300

    0.1. Statement [#379306 #504527]

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -30,61 +30,51 @@ public class TableEditor implements AutoCloseable {
 
     }
 
-    public void createTable(String tableName) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "CREATE TABLE IF NOT EXISTS %s(%s, %s);",
-                    tableName,
-                    "id SERIAL PRIMARY KEY",
-                    "name TEXT"
-            );
-            statement.execute(sql);
-        }
+    public void createTable(String tableName) {
+        String sql = String.format(
+                "CREATE TABLE IF NOT EXISTS %s(%s, %s);",
+                tableName,
+                "id SERIAL PRIMARY KEY",
+                "name TEXT"
+        );
+        oneQueryExecute(sql);
     }
 
-    public void dropTable(String tableName) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "DROP TABLE %s;",
-                    tableName
-            );
-            statement.execute(sql);
-        }
+    public void dropTable(String tableName) {
+        String sql = String.format(
+                "DROP TABLE %s;",
+                tableName
+        );
+        oneQueryExecute(sql);
     }
 
-    public void addColumn(String tableName, String columnName, String type) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "ALTER TABLE %s ADD COLUMN %s %s;",
-                    tableName,
-                    columnName,
-                    type
-            );
-            statement.execute(sql);
-        }
+    public void addColumn(String tableName, String columnName, String type) {
+        String sql = String.format(
+                "ALTER TABLE %s ADD COLUMN %s %s;",
+                tableName,
+                columnName,
+                type
+        );
+        oneQueryExecute(sql);
     }
 
-    public void dropColumn(String tableName, String columnName) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "ALTER TABLE %s DROP COLUMN %s CASCADE;",
-                    tableName,
-                    columnName
-            );
-            statement.execute(sql);
-        }
+    public void dropColumn(String tableName, String columnName) {
+        String sql = String.format(
+                "ALTER TABLE %s DROP COLUMN %s CASCADE;",
+                tableName,
+                columnName
+        );
+        oneQueryExecute(sql);
     }
 
-    public void renameColumn(String tableName, String columnName, String newColumnName) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "ALTER TABLE %s RENAME COLUMN %s TO %s;",
-                    tableName,
-                    columnName,
-                    newColumnName
-            );
-            statement.execute(sql);
-        }
+    public void renameColumn(String tableName, String columnName, String newColumnName) {
+        String sql = String.format(
+                "ALTER TABLE %s RENAME COLUMN %s TO %s;",
+                tableName,
+                columnName,
+                newColumnName
+        );
+        oneQueryExecute(sql);
     }
 
 
@@ -111,6 +101,14 @@ public class TableEditor implements AutoCloseable {
     public void close() throws SQLException {
         if (connection != null) {
             connection.close();
+        }
+    }
+
+    private void oneQueryExecute(String sql) {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
