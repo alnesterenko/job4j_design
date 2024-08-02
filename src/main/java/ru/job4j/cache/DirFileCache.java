2ad75f969a90e8d9c8d07823b5822291b9fa4947
alnesterenko <alzpupkin@gmail.com>
2024-08-02 19:11:40 +0300

    1. Реализация кеша на SoftReference [#1592]

---
+++ src/main/java/ru/job4j/cache/DirFileCache.java
@@ -0,0 +1,29 @@
+package ru.job4j.cache;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.StringJoiner;
+import java.util.stream.Stream;
+
+public class DirFileCache extends AbstractCache<String, String> {
+
+    private final String cachingDir;
+
+    public DirFileCache(String cachingDir) {
+        this.cachingDir = cachingDir;
+    }
+
+    @Override
+    public String load(String key) {
+        StringJoiner stringJoiner = new StringJoiner(System.lineSeparator());
+        try (Stream<String> lines = Files.lines(Paths.get(cachingDir + "/" + key))) {
+            lines.forEach(stringJoiner::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String finalString = stringJoiner.toString();
+        super.put(key, finalString);
+        return finalString;
+    }
+}
