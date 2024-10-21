763fa94a2cbfa68ca4d0e71ae1d2f45b02fe1964
alnesterenko <alzpupkin@gmail.com>
2024-10-21 18:51:08 +0300

    Использование Stack-а для правильного превращения пути в Линукс в укороченный формат.

---
+++ src/main/java/ru/job4j/algo/stack/Path.java
@@ -0,0 +1,27 @@
+package ru.job4j.algo.stack;
+
+import java.util.Stack;
+
+class Path {
+    public String simplify(String path) {
+        var stack = new Stack<String>();
+        var components = path.split("/");
+        for (var component : components) {
+            if (component.equals("..")) {
+                stack.pop();
+            } else if (!component.isEmpty() && !component.equals(".")) {
+                stack.push(component);
+            }
+        }
+        var result = new StringBuilder();
+        for (String dir : stack) {
+            result.append("/").append(dir);
+        }
+        return !result.isEmpty() ? result.toString() : "/";
+    }
+
+    public static void main(String[] args) {
+        Path path = new Path();
+        System.out.println(path.simplify("///c/home//.../user/..//usver/"));
+    }
+}
