9fbf4bb8b111de4ce60c4487f1a9a258eb576a25
alnesterenko <alzpupkin@gmail.com>
2024-12-07 15:48:24 +0200

    Проверка зацикливания дерева без тестов.

---
+++ src/main/java/ru/job4j/algo/exam/cicletree/Node.java
@@ -0,0 +1,58 @@
+package ru.job4j.algo.exam.cicletree;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class Node<E> {
+    private E value;
+    private List<Node<E>> children = new ArrayList();
+
+    public Node(E value) {
+        this.value = value;
+    }
+
+    @SafeVarargs
+    public Node(E value, Node<E>... children) {
+        this.value = value;
+        this.children.addAll(Arrays.asList(children));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Node{ %s }", value);
+    }
+
+    public E getValue() {
+        return value;
+    }
+
+    public void setValue(E value) {
+        this.value = value;
+    }
+
+    public List<Node<E>> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Node<E>> children) {
+        this.children = children;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Node<?> node)) {
+            return false;
+        }
+        return Objects.equals(getValue(), node.getValue()) && Objects.equals(getChildren(), node.getChildren());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue(), getChildren());
+    }
+}
---
+++ src/main/java/ru/job4j/algo/exam/cicletree/TreeUtils.java
@@ -0,0 +1,179 @@
+package ru.job4j.algo.exam.cicletree;
+
+
+import ru.job4j.algo.exam.cicletree.Node;
+import ru.job4j.collection.SimpleQueue;
+import ru.job4j.collection.SimpleStack;
+
+import java.util.*;
+
+public class TreeUtils<T> {
+
+    /**
+     * Метод выполняет обход дерева и считает количество узлов
+     * @param root корневой узел дерева
+     * @return количество узлов
+     */
+    public int countNode(Node<T> root) {
+        return findAllValues(root).size();
+    }
+
+    /**
+     * Метод выполняет обход дерева и возвращает коллекцию ключей узлов дерева
+     * @param root корневой узел
+     * @return коллекция с ключами, реализующая интерфейс Iterable<E>
+     */
+    public Iterable<T> findAll(Node<T> root) {
+        return findAllValues(root);
+    }
+
+    /**
+     * Служебный метод выполняет обход дерева и возвращает коллекцию(список) ключей узлов дерева
+     * @param root корневой узел
+     * @return коллекция(список) с ключами, реализующая интерфейс Iterable<E>
+     * @throws IllegalArgumentException если root является null
+     */
+    private List<T> findAllValues(Node<T> root) {
+        SimpleQueue<Node<T>> queue = new SimpleQueue<>();
+        List<T> listOfNodes = new ArrayList<>();
+        if (root == null) {
+            throw new IllegalArgumentException();
+        }
+        queue.push(root);
+        while (!queue.isEmpty()) {
+            Node<T> tempNode = queue.poll();
+            listOfNodes.add(tempNode.getValue());
+            for (Node<T> oneChild : tempNode.getChildren()) {
+                queue.push(oneChild);
+            }
+        }
+        return listOfNodes;
+    }
+
+
+    /**
+     * Метод обходит дерево root и добавляет к узлу с ключом parent
+     * новый узел с ключом child, при этом на момент добавления узел с ключом parent
+     * уже должен существовать в дереве, а узла с ключом child в дереве быть не должно
+     * @param root корень дерева
+     * @param parent ключ узла-родителя
+     * @param child ключ узла-потомка
+     * @return true если добавление произошло успешно и false в обратном случае.
+     */
+    public boolean add(Node<T> root, T parent, T child) {
+        boolean success = false;
+        Optional<Node<T>> result = findByKey(root, parent);
+        if (result.isPresent()) {
+            List<Node<T>> children = result.get().getChildren();
+            Node<T> newChild = new Node<>(child);
+            if (!children.contains(newChild)) {
+                success = children.add(newChild);
+            }
+        }
+        return success;
+    }
+
+    /**
+     * Метод обходит дерево root и возвращает первый найденный узел с ключом key
+     * @param root корень дерева
+     * @param key ключ поиска
+     * @return узел с ключом key, завернутый в объект типа Optional
+     */
+    public Optional<Node<T>> findByKey(Node<T> root, T key) {
+        return findOrDivideByKey(root, key, false);
+    }
+
+    /**
+     * Метод обходит дерево root и возвращает первый найденный узел с ключом key,
+     * при этом из дерева root удаляется все поддерево найденного узла
+     * @param root корень дерева
+     * @param key ключ поиска
+     * @return узел с ключом key, завернутый в объект типа Optional
+     */
+    public Optional<Node<T>> divideByKey(Node<T> root, T key) {
+        return findOrDivideByKey(root, key, true);
+    }
+
+    /**
+     * Служебный метод обходит дерево root и возвращает первый найденный узел с ключом key,
+     * если параметр needDelete == true, из дерева root удаляется все поддерево найденного узла
+     * @param root корень дерева
+     * @param key ключ поиска
+     * @param needDelete нужно ли удалять найденный элемент и всё его поддерево
+     * @return узел с ключом key, завернутый в объект типа Optional
+     * @throws IllegalArgumentException если root является null
+     */
+    private Optional<Node<T>> findOrDivideByKey(Node<T> root, T key, boolean needDelete) {
+        /* Да, возможно, немного "наговнокодил" в этом методе, но зато код не дублируется в двух предыдущих методах )))) */
+        SimpleStack<Node<T>> stack = new SimpleStack<>();
+        Optional<Node<T>> result = Optional.empty();
+        int count = 0;
+        boolean wasFound = false;
+        if (root == null) {
+            throw new IllegalArgumentException();
+        }
+        if (root.getValue().equals(key)) {
+            result = Optional.of(root);
+        } else {
+            stack.push(root);
+            count++;
+            while (count != 0) {
+                Node<T> tempNode = stack.pop();
+                count--;
+                List<Node<T>> children = tempNode.getChildren();
+                if (children.size() > 0) {
+                    for (Node<T> oneChild : children) {
+                        if (oneChild.getValue().equals(key)) {
+                            result = Optional.of(oneChild);
+                            if (needDelete) {
+                                children.remove(oneChild);
+                            }
+                            wasFound = true;
+                            break;
+                        }
+                        stack.push(oneChild);
+                        count++;
+                    }
+                    if (wasFound) {
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private List<Node<T>> findAllNodes(Node<T> root) {
+        SimpleQueue<Node<T>> queue = new SimpleQueue<>();
+        List<Node<T>> listOfNodes = new ArrayList<>();
+        if (root == null) {
+            throw new IllegalArgumentException();
+        }
+        queue.push(root);
+        while (!queue.isEmpty()) {
+            Node<T> tempNode = queue.poll();
+            listOfNodes.add(tempNode);
+            for (Node<T> oneChild : tempNode.getChildren()) {
+                queue.push(oneChild);
+            }
+        }
+        return listOfNodes;
+    }
+
+    /* Время здесь квадратичное */
+    public Optional<Node<T>> checkerCycle(Node<T> root) {
+        Optional<Node<T>> result = Optional.empty();
+        Set<Node<T>> tempSet = new HashSet<>();
+        List<Node<T>> tempList = findAllNodes(root);
+        for (Node oneNode : tempList) {
+            if (!tempSet.add(oneNode)) {
+                for (Node oneNodeInSet : tempSet) {
+                    if (Objects.equals(oneNode, oneNodeInSet)) {
+                        return Optional.of(oneNode);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
